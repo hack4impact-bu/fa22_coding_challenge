ca90013774bf1662e7f60b145ef943b81d82126d
ArkashJ <arkjain@bu.edu>
2022-09-22 20:13:47 -0400

    gave up previous approach, tyring something new, made the schema and connected mongo

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,22 @@
 const cors = require("cors");
 const express = require("express");
 const app = express();
-// const connection = require("./db");
-// const tasks = require("./routes/tasks");
 
-// connection();
+/*
+Note to self - could use import as well since import 
+is asynchronous and it works a little better
+*/
 
-// app.use(cors());
-// app.use(express.json());
-// app.use("/api/tasks", tasks);
+const connection = require("./db");
+const tasks = require("./routes/tasks");
 
+connection();
 const port = process.env.PORT || 8080;
 
-app.listen(port, () => console.log(`Listening on port ${port}...`));
+app.use(cors());
+app.use(express.json());
 
-export default app
+app.use("/api/tasks", tasks);
+app.use("*", (req,res)=> res.status(404).json({error: "not found"}))
+
+app.listen(port, () => console.log(`Listening on port ${port}...`));
