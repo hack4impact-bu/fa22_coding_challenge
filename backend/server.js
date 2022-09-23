f7730fb1b8b860021ce814e777392b72dfab8052
ArkashJ <arkjain@bu.edu>
2022-09-22 20:47:37 -0400

    finished adding the api calls, semantic changes in the server file for personal understanding

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,17 @@
 const cors = require("cors");
 const express = require("express");
 const app = express();
-
+//imports
+const connection = require("./db");
+const tasks = require("./routes/tasks");
 /*
 Note to self - could use import as well since import 
 is asynchronous and it works a little better
 */
 
-const connection = require("./db");
-const tasks = require("./routes/tasks");
-
+//run mongo
 connection();
+//set port
 const port = process.env.PORT || 8080;
 
 app.use(cors());
